721cbc05ad96c44a66142b3cf290f8e91777a23c
jasperxodiac <114020124+jasperxodiac@users.noreply.github.com>
2022-11-24 20:57:18 +0530

    Update text.c

--- text.c
+++ text.c
@@ -1,28 +1,57 @@
-#include <stdio.h>
+#define _GNU_SOURCE
 #include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <sys/types.h>
 
-int main(){
-    float array1[4][4] = {{1.0,2.0,3.0,4.0},{5.0,6.0,7.0,8.0},{9.0,10.0,11.0,12.0},{13.0,14.0,15.0,16.0}};
-    float array2[4][4];
+#define kernel_2D_memcpy 451
 
-    int res = syscall(451,array1,array2,4,4);
-    printf("System Before Result: %d\n",res);
-    for(int i =0;i<4;i++){
-        for(int j=0;j<4;j++){
-            printf("%f ",array1[i][j]);
-        }
+int main() {
+    float MAT1[4][4] = {{1.0,2.0,3.0},{3.0,4.0,5.0},{5.0,6.0,7.0},{7.0,8.0,9.0}};
+    float MAT2[4][4] = {{1.0,1.0,1.0},{1.0,1.0,1.0},{1.0,1.0,1.0},{1.0,1.0,1.0}};
+
+    printf("This is the initial matrix1 is:\n");
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            printf("%lf ", MAT1[i][j]);
+            }
         printf("\n");
     }
-    
-    printf("System Call Result: %d\n",res);
-    
-    for(int i =0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            printf("Array Elemnet %d%d: %f\n",i,j,array2[i][j]);
-        }
+    printf("\n");
+
+    printf("This is the initial matrix2 is:\n");
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            printf("%lf ", MAT2[i][j]);
+            }
+        printf("\n");
     }
+    printf("\n");
+
+    int res = syscall(kernel_2D_memcpy, MAT1, MAT2, 4, 4);
+
+
+    printf("This is the final matrix1 is:\n");
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            printf("%lf ", MAT1[i][j]);
+            }
+        printf("\n");
+    }
+    printf("\n");
+
+    printf("This is the final matrix2 is:\n");
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            printf("%lf ", MAT2[i][j]);
+            }
+        printf("\n");
+    }
+    printf("\n");
+
     return 0;
 }
